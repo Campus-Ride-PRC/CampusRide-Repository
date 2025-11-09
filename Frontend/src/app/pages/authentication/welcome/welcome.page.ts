aa0b088a5fdac061e9ec6de361bcca10c301942c
Stefan <stefanpintilie46@gmail.com>
2025-11-09 14:38:53 +0200

    [frontend] Added Card Component

--- Frontend/src/app/pages/authentication/welcome/welcome.page.ts
+++ Frontend/src/app/pages/authentication/welcome/welcome.page.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { PrimaryButtonComponent } from '../../../shared/components/buttons/primary-button.component';
+import { RideCardComponent } from '../../../shared/components/cards/ride-card.component';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [CommonModule, IonicModule, PrimaryButtonComponent],
+  imports: [CommonModule, IonicModule, PrimaryButtonComponent, RideCardComponent],
   templateUrl: './welcome.page.html',
   styleUrls: ['./welcome.page.scss'],
 })
@@ -14,5 +15,9 @@ export class WelcomePage {
   onLogin() {
     // Handle login navigation
     console.log('Login clicked');
+  }
+
+  onRideCardClick() {
+    console.log('Ride card clicked');
   }
 }
