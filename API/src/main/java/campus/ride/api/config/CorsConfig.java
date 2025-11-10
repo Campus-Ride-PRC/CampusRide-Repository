5722e421ded2d3ff7f22ec738b3d1a4ea0067cc9
raulemm7 <86725051+raulemm7@users.noreply.github.com>
2025-11-10 22:20:24 +0200

    finale-changes

--- API/src/main/java/campus/ride/api/config/CorsConfig.java
+++ API/src/main/java/campus/ride/api/config/CorsConfig.java
@@ -14,10 +14,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200") // frontend origin
+                        .allowedOrigins("*") // frontend origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*");
             }
         };
     }
